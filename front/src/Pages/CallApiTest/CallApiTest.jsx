af2bec3863904d395cdd6fc00d7f7bc11d6478dd
KangByungChanFront <fullstackkang@gmail.com>
2023-09-07 22:53:38 +0900

    fetch -> axios change in api.js

--- front/src/Pages/CallApiTest/CallApiTest.jsx
+++ front/src/Pages/CallApiTest/CallApiTest.jsx
@@ -4,13 +4,13 @@ import axios from 'axios'
 
 
 function CallApiTest() {
-  const [apiData, setApiData] = useState();
+  const [text, setText] = useState('');
   const [Riot, setRiot] = useState(null);
   // const getApi = async () => {
   //   axios.get("/api").then(res => setApiData(res.data));
   // }
-  const postApi = async () => {
-    axios.post("http://localhost:5000/api/summoner/hide on bush")
+  const postApi = async (name) => {
+    axios.post(`http://localhost:5000/api/summoner/${name}`)
     .then((response) => {
       if (!response.status) {
         throw new Error('서버 응답 오류: ' + response.status);
@@ -21,15 +21,23 @@ function CallApiTest() {
       console.error('Error:', error);
     });;
   }
+
+  const getInput = e => {
+    console.log(e.target.value);
+    setText(`${e.target.value}`)
+  }
+
   useEffect(() => {
     console.log(Riot);
   },[Riot])
   return (
     <>
       <h1>안녕하세요 프론트 페이지 입니다.</h1>
-      <div>데이터 : {apiData}</div>
       
-      <button onClick={postApi}>눌러라</button>
+      {/* <button onClick={postApi}>눌러라</button> */}
+      <input type="text" onChange={getInput} onKeyDown={e => {
+        if (e.key === 'Enter') postApi(e.target.value);
+      }}/>
     </>
   );
 }
